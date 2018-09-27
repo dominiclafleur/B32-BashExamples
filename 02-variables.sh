6cc1742e86fce42c510ba669337e3abb77ab8832
Dominic <irqubn@gmail.com>
2018-09-27 14:18:21 -0400

    Done with variables!

--- 02-variables.sh
+++ 02-variables.sh
@@ -1,2 +1,19 @@
 #!/bin/bash
 
+# Assignation (sans $)
+prenom=David 
+
+# Lecture de la variable, toujours avec $
+echo $prenom
+
+declare -i age=33 # variable est un entier
+declare -r age2=34 # variable en lecture seule, impossible de réaffecter
+
+cours="Cours de Linux"
+
+# Les variables systèmes:
+# ./02-variables.sh fred test
+echo Prog $0 avec premier param $1, deuxième param $2, total est $# param
+
+
+
