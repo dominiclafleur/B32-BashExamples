fd46360a66157492a6a3f4fada8d36b33ed87112
Dominic <irqubn@gmail.com>
2018-09-27 16:12:27 -0400

    Done with exercise 2

--- exercice2.sh
+++ exercice2.sh
@@ -1,1 +1,27 @@
 #!/bin/bash
+
+afficherMenu() {
+	echo "--------------------"
+	echo "d = dictionnaire"
+	echo "q = quitter"
+	echo "--------------------"
+}
+
+choix=a
+
+while test $choix != q;do
+	afficherMenu
+
+	read -p "Entrez votre choix : " choix
+
+	if test $choix = d;then
+		read -p "Entrez un mot: " mot
+		./exercice1.sh $mot
+		echo ""
+	elif test $choix = q;then
+		exit
+	else
+		echo "Le choix est invalide."
+		echo ""
+	fi
+done
