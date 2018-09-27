b9a1583e4b5e49983c29a155425be240b688e949
Dominic <irqubn@gmail.com>
2018-09-27 14:06:35 -0400

    Finished hello world file

--- 01-hello_world.sh
+++ 01-hello_world.sh
@@ -1,2 +1,23 @@
 #!/bin/bash
 
+# Je suis un commentaire!
+
+echo "Salut!"
+
+# exit
+# echo "On ne me verra jamais :( "
+
+# -n = ne pas faire de retour de chariot
+echo -n "Vive "
+echo "Linux"
+
+echo -e "1\n2\n3\n"
+
+read -p "Quel âge avez-vous : " age
+echo $age "ans! Vous paraissez plus jeune!"
+
+echo -n "Question : "
+read resultat
+echo $resultat
+
+
