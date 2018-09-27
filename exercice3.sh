bf54a363fe27e2c25a7dabb92f5ab2d87b1221cb
Dominic <irqubn@gmail.com>
2018-09-27 16:49:35 -0400

    Finished exercise 3

--- exercice3.sh
+++ exercice3.sh
@@ -1,1 +1,23 @@
+
 #!/bin/bash
+
+clear
+
+addSpace() {
+	for ((i=0;i<$1;i++));do
+		echo -n "            "
+	done
+}
+
+for ((i=0;i<5;i++));do
+	addSpace $i
+	echo -n "@"
+	sleep 0.2
+	clear
+
+	if test $i -eq 4;then
+		addSpace $i
+		echo -n "BOOOOOOOOOOOM"
+		read dummy
+	fi
+done
