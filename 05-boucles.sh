ee488101bccbd850ce9fce37f8ff4d762f5ecbee
Dominic <irqubn@gmail.com>
2018-09-27 14:55:14 -0400

    Done with loops!

--- 05-boucles.sh
+++ 05-boucles.sh
@@ -1,2 +1,23 @@
 #!/bin/bash
 
+for ((i=0;i<5;i++))
+do
+	echo $i
+done
+
+liste=`ls` # accent grave permet d'exécuter une commande
+
+for element in  $liste
+do
+	echo $element
+done
+
+user=`whoami`
+echo "salut $user"
+
+resultat=o
+
+# -o = OR, -a = AND
+while test $resultat = o -o $resultat = a;do
+	read -p "Entrez une lettre : " resultat
+done
